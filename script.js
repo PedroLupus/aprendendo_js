158dfad0f7d86113a7e53c75ecf90d0c069cb546
PedroLupus <izuraph07@gmail.com>
2026-09-22 19:26:30 -0300

    adicionado .js

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnTrocaCor = document.getElementById('btnTrocaCor');
+
+btnTrocaCor.addEventListener('click', () => {
+
+  const corAleatoria = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  
+  document.body.style.backgroundColor = corAleatoria;
+});
